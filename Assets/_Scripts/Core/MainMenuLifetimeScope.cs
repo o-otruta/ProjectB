c0ce541056c55ed0a4dc63d082ce0db9e885bbdc
o-otruta <firefly322x@gmail.com>
2026-09-04 21:25:41 +0300

    fix(di): remove redundant manual injection in MainMenuLifetimeScope

--- Assets/_Scripts/Core/MainMenuLifetimeScope.cs
+++ Assets/_Scripts/Core/MainMenuLifetimeScope.cs
@@ -10,48 +10,11 @@ namespace ProjectB.Core
     {
         protected override void Configure(IContainerBuilder builder)
         {
-            Debug.Log("[MainMenuLifetimeScope] Configure started");
             builder.RegisterComponentInHierarchy<MainMenuUI>();
             builder.RegisterComponentInHierarchy<MetaUpgradeUI>();
             builder.RegisterComponentInHierarchy<AchievementScreenUI>();
             builder.RegisterComponentInHierarchy<DailyBonusScreenUI>();
             builder.Register<DailyBonusManager>(Lifetime.Scoped);
-
-            builder.RegisterBuildCallback(resolver =>
-            {
-                Debug.Log("[MainMenuLifetimeScope] BuildCallback executed");
-                var ui = FindAnyObjectByType<MainMenuUI>(FindObjectsInactive.Include);
-                if (ui != null)
-                {
-                    Debug.Log("[MainMenuLifetimeScope] Found MainMenuUI, injecting!");
-                    resolver.Inject(ui);
-                }
-                else
-                {
-                    Debug.LogWarning("[MainMenuLifetimeScope] MainMenuUI NOT found in scene!");
-                }
-
-                var metaUi = FindAnyObjectByType<MetaUpgradeUI>(FindObjectsInactive.Include);
-                if (metaUi != null)
-                {
-                    Debug.Log("[MainMenuLifetimeScope] Found MetaUpgradeUI, injecting!");
-                    resolver.Inject(metaUi);
-                }
-                
-                var achievementUi = FindAnyObjectByType<AchievementScreenUI>(FindObjectsInactive.Include);
-                if (achievementUi != null)
-                {
-                    Debug.Log("[MainMenuLifetimeScope] Found AchievementScreenUI, injecting!");
-                    resolver.Inject(achievementUi);
-                }
-
-                var bonusUi = FindAnyObjectByType<DailyBonusScreenUI>(FindObjectsInactive.Include);
-                if (bonusUi != null)
-                {
-                    Debug.Log("[MainMenuLifetimeScope] Found DailyBonusScreenUI, injecting!");
-                    resolver.Inject(bonusUi);
-                }
-            });
         }
     }
 }
